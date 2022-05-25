605c8fc660384dee9aad59283ed4e34b1d279c17
nigulasi-zhao <71370733+nigulasi-zhao@users.noreply.github.com>
2022-05-25 17:03:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'throbbing-shape-326d.hy961437476.workers.dev'
+      url.hostname = 'simawenlong.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
