4238e879fb778008d827d9957c5fbc8d191114c1
nigulasi-zhao <71370733+nigulasi-zhao@users.noreply.github.com>
2022-05-25 16:41:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'nigulasizhaosi.ml'
+      url.hostname = 'throbbing-shape-326d.hy961437476.workers.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
